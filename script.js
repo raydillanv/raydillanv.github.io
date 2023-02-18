dbed94a93a6ac9f299726a61c0fcff3e0fcf9ed5
Dillan Victory <raydillanv1@gmail.com>
2023-02-17 18:11:22 -0600

    Undo

--- script.js
+++ script.js
@@ -1,26 +1,14 @@
-function updateFileList() {
-  fetch('files.json')
-    .then(response => response.json())
-    .then(fileList => {
-      // Get the list of files in the folder
-      fetch('/listFolder')
-        .then(response => response.json())
-        .then(folderList => {
-          const addedFiles = folderList.filter(file => !fileList.some(existingFile => existingFile.name === file.name));
-          if (addedFiles.length > 0) {
-            const updatedFileList = [...fileList, ...addedFiles];
-            // Write the updated file list back to files.json
-            fetch('files.json', {
-              method: 'POST',
-              headers: {
-                'Content-Type': 'application/json'
-              },
-              body: JSON.stringify(updatedFileList)
-            });
-          }
-        });
-    });
-}
+const list = document.getElementById('file-list');
 
-// Update the file list every 5 seconds
-setInterval(updateFileList, 5000);
+fetch('files.json')
+  .then(response => response.json())
+  .then(fileList => {
+    fileList.forEach(file => {
+      const li = document.createElement('li');
+      const a = document.createElement('a');
+      a.href = file.url;
+      a.textContent = file.name;
+      li.appendChild(a);
+      list.appendChild(li);
+    });
+  });
