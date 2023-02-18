0b2c244305e48df14e6fbf12352a90eb273600f2
Dillan Victory <raydillanv1@gmail.com>
2023-02-17 18:16:43 -0600

    check

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const list = document.getElementById('file-list');
 
-fetch('files.json')
+fetch(window.location.href.replace('listFolder.html', 'files.json') + '?_=' + new Date().getTime())
   .then(response => response.json())
   .then(fileList => {
+    list.innerHTML = ''; // Remove existing list items
     fileList.forEach(file => {
       const li = document.createElement('li');
       const a = document.createElement('a');
